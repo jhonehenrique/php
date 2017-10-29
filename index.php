15a87a89fc8d7a94b3ae0e89bafd8073de6818e0
jhonehenrique <jhone.henrique.info@gmail.com>
2017-10-29 16:55:16 -0200

    gerando conflito

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-	echo "Olá mundo com conflito!";
-=======
 	echo "Olá mundo Git!";
 
 	echo "Ola Xbyte!"
->>>>>>> 4c9d3aa025f2d3c4342ff10ae8c7f69980cd5889
-
-	echo "Olá Udemy!";
 ?>
